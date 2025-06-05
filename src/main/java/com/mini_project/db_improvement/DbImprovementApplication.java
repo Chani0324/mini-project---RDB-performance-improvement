b9a3d20346f9f1811c3c476cb0c23bb11d319f18
HongChan Yoon <hongchani0324@gmail.com>
2025-06-05 18:55:29 +0900

    init commit

---
+++ src/main/java/com/mini_project/db_improvement/DbImprovementApplication.java
@@ -0,0 +1,13 @@
+package com.mini_project.db_improvement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbImprovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbImprovementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mini_project/db_improvement/DbImprovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini_project.db_improvement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbImprovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
